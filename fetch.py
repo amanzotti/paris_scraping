0da6189468f16d64070f46f07b3b903d590c405b
manzotti <manzotti.alessandro@gmail.com>
2017-03-24 00:09:30 +0000

    minor changes and internal del references

--- fetch.py
+++ fetch.py
@@ -9,10 +9,31 @@ from sortedcontainers import SortedDict
 import datetime
 import bs4
 
+# TODO
+
+# http://www.meilleursagents.com/immobilier/recherche/?item_types%5B%5D=369681781&item_types%5B%5D=369681782&transaction_type=369681778&place_ids%5B%5D=32696
+
+# http://www.seloger.com/list.htm?idtt=1&idtypebien=1&cp=75&tri=initial
+
 
 def parse_source(html, encoding='utf-8'):
     parsed = BeautifulSoup(html, from_encoding=encoding)
     return parsed
+
+
+def fetch_meilleursagents():
+    base = 'http://www.meilleursagents.com/immobilier/recherche/?redirect_url=&view_mode=list&sort_mode=ma_contract%7Cdesc&transaction_type=369681778&buyer_search_id=&user_email=&place_ids%5B%5D=138724240&place_title=&item_types%5B%5D=369681781&item_types%5B%5D=369681782&item_area_min=&item_area_max=&budget_min=&budget_max='
+    resp = requests.get(base, timeout=15)
+    resp.raise_for_status()  # <- no-op if status==200
+    parsed = parse_source(resp.content, resp.encoding)
+
+
+
+def fetch_solger():
+    base = 'http://www.seloger.com/list.htm?idtt=1&idtypebien=1&cp=75&tri=initial'
+    resp = requests.get(base, timeout=15)
+    resp.raise_for_status()  # <- no-op if status==200
+    parsed = parse_source(resp.content, resp.encoding)
 
 
 def fetch_pap():
@@ -290,6 +311,112 @@ def extract_listings_pap(parsed):
         }
         extracted.append(SortedDict(this_listing))
     return extracted
+
+
+
+def extract_listings_solger(parsed):
+    # location_attrs = {'data-latitude': True, 'data-longitude': True}
+    listings = parsed.find_all(
+        'article', {'class': "listing life_annuity  gold"})
+    extracted = []
+    return listings
+    # for listing in listings[0:]:
+    #     # hood = listing.find('span', {'class': 'result-hood'})
+    #     # # print(hood)
+    #     # # location = {key: listing.attrs.get(key, '') for key in location_attrs}
+    #     # link = listing.find('a', {'class': 'result-title hdrlnk'})  # add this
+    #     # if link is not None:
+    #     #     descr = link.string.strip()
+    #     #     link_href = link.attrs['href']
+
+    #     price = listing.find('span', {'class': 'price'})
+    #     if price is not None:
+    #         price = float(price.string.split()[0].replace('.', ''))
+
+    #     ref = listing.find('div', {'class': 'float-right'}).find('a', href=True)['href']
+    #     base = 'http://www.pap.fr/' + ref
+    #     resp = requests.get(base, timeout=20)
+    #     link = base
+    #     resp.raise_for_status()  # <- no-op if status==200
+
+    #     resp_comb = parse_source(resp.content, resp.encoding)
+    #     descr = resp_comb.find_all('p', {'class': 'item-description'})[0]
+    #     desc = ' '
+    #     for line in descr.contents:
+    #         if isinstance(line, bs4.element.NavigableString):
+    #             desc += ' ' + line.string.strip('<\br>').strip('\n')
+
+    #     # return resp_comb.find_all(
+    #     #     'ul', {'class': 'item-summary'})
+
+    #     try:
+    #         ars = int(resp_comb.find(
+    #             'div', {'class': 'item-geoloc'}).find('h2').string.split('e')[0][-2:])
+    #     except:
+    #         break
+
+    #     # return resp_comb.find_all('ul', {'class': 'item-summary'})[0].find_all('li')
+
+    #     # print(resp_comb.find_all('ul', {'class': 'item-summary'})[0].find_all('li'))
+    #     temp_dict_ = {}
+    #     for lines in resp_comb.find_all('ul', {'class': 'item-summary'})[0].find_all('li'):
+    #         tag = lines.contents[0].split()[0]
+    #         value = int(lines.find_all('strong')[0].string.split()[0])
+    #         temp_dict_[tag] = value
+
+    #     try:
+    #         pieces = temp_dict_[u'Pi\xe8ces']
+    #     except:
+    #         pieces = None
+    #     try:
+    #         chambre = temp_dict_[u'Chambre']
+    #     except:
+    #         chambre = None
+    #     try:
+    #         square_meters = temp_dict_['Surface']
+
+    #     except:
+    #         square_meters = None
+
+    #     # meters = resp_comb.find_all('ul', {'class': 'item-summary'}
+    #     #                             )[0].find_all('strong').string.split()[0]
+
+    #     # link = listing.find('div', {'class': 'listos'}).find('a',href=True)['href']
+
+    #     # resp = requests.get(link, timeout=10)
+    #     # resp.raise_for_status()  # <- no-op if status==200
+
+    #     # desc = listing.find('p', {'class': 'post-desc'}
+    #     #                     )
+    #     # if price is not None:
+    #     #     desc = desc.string
+
+    #     # housing = listing.find('span', {'class': 'housing'})
+    #     # if housing is not None:
+    #     #     beds = housing.decode_contents().split('br')[0][-1]
+    #     #     rm = housing.decode_contents().split('m<sup>2</sup>')[0]
+    #     #     sqm = [int(s) for s in rm.split() if s.isdigit()]
+    #     #     if len(sqm) == 0:
+    #     #         sqm = None
+    #     #     else:
+    #     #         sqm = int(sqm[0])
+
+    #     this_listing = {
+    #         # 'location': location,
+    #         # 'link': link_href,                    # add this too
+    #         # 'description': descr,            # and this
+    #         'price': price,
+    #         'desc': desc,
+    #         'pieces': pieces,
+    #         'meters': square_meters,
+    #         'chambre': chambre,
+    #         'ars': ars,
+    #         # 'meters': sqm,
+    #         # 'beds': beds
+    #         'link': link
+    #     }
+    #     extracted.append(SortedDict(this_listing))
+    # return extracted
 
 # parsed.find_all(
 #     ...:         'div', {'class': "box search-results-item"})[0].find('div',{'class':'float-right'}).find('a',href=True)['href']
